8fb43ee9f3414ac71e79bf03cb24c24ef915f483
Wahyu Kristiawan <whykris97@gmail.com>
2022-04-27 12:38:28 +0700

    edit migration

--- app/Database/Seeds/InitApp.php
+++ app/Database/Seeds/InitApp.php
@@ -8,6 +8,14 @@ class InitApp extends Seeder
 {
     public function run()
     {
-        //
+        $this->call('App\Database\Seeds\App\FinancialAccount');
+
+        // check env
+        $env = getenv('app.pos_type');
+        if ($env == 'simple') {
+            $this->call('App\Database\Seeds\App\SettingSimple');
+        } else {
+            $this->call('App\Database\Seeds\App\SettingComplex');
+        }
     }
 }
