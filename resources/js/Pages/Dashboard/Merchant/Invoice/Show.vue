b89aa10afe5188f1599bc562234dab4754fafb8b
Wahyu Kristiawan <whykris97@gmail.com>
2025-10-06 18:33:30 +0700

    delete cache php-cs-fixer

---
+++ resources/js/Pages/Dashboard/Merchant/Invoice/Show.vue
@@ -0,0 +1,156 @@
+<template>
+    <Container>
+        <div class="flex flex-col border rounded-lg bg-white p-2 h-full gap-4">
+            <div class="inline-flex justify-between">
+                <div class="text-lg font-medium">
+                    Invoice #{{ invoice.code }}
+                    <div class="text-sm">
+                        <div>
+                            Tanggal :
+                            {{ formatDateTimeID(invoice.created_at) }}
+                        </div>
+                        <div>Metode Pembayaran : -</div>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <label
+                        v-if="invoice.status === 'unpaid'"
+                        class="badge text-xl badge-warning"
+                        >Belum Dibayar</label
+                    >
+                    <label
+                        v-else-if="invoice.status === 'paid'"
+                        class="badge text-xl badge-success"
+                        >Terbayar</label
+                    >
+                    <label
+                        v-else-if="invoice.status === 'canceled'"
+                        class="badge text-xl badge-danger"
+                        >Dibatalkan</label
+                    >
+                    <label v-else class="badge text-xl badge-gray-400"
+                        >Expired</label
+                    >
+                </div>
+            </div>
+
+            <div class="grid grid-cols-2 gap-4">
+                <div class="border gap-2 p-2 rounded-lg">
+                    <div class="font-medium">Dibayarkan Kepada :</div>
+                    <div class="text-sm">
+                        PT. SOLUSI DARI ANAK BANGSA <br />
+                        NPWP 1000 0000 0546 70
+                    </div>
+                </div>
+                <div class="border gap-2 p-2 rounded-lg">
+                    <div class="font-medium">Ditagihkan Kepada :</div>
+                    <div class="text-sm">
+                        {{ invoice.merchant.name }} <br />
+                        {{ invoice.merchant.owner_name }} <br />
+                        {{ invoice.merchant.address }}
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <div class="font-medium">Item</div>
+                <div class="grid grid-cols-6 text-sm gap-y-2">
+                    <div class="col-span-5 font-medium">Deskripsi</div>
+                    <div class="font-medium">Jumlah</div>
+
+                    <div class="col-span-6">
+                        <hr />
+                    </div>
+                    <div class="col-span-5">
+                        {{ invoice.plan.name }} ({{
+                            invoice.plan.duration
+                        }}
+                        hari) <br />
+                        {{ invoice.note }} <br />
+                        <div v-for="item in invoice.items">
+                            - {{ item.outlet.name }}
+                        </div>
+                    </div>
+                    <div class="">
+                        {{ formatIDR(invoice.subtotal) }}
+                    </div>
+                    <div class="col-span-6">
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <template #footer>
+            <div class="flex justify-between gap-2">
+                <div>
+                    <div>
+                        <button class="btn btn-main">
+                            <FontAwesomeIcon :icon="faDownload" />
+                            Download Invoice
+                        </button>
+                    </div>
+                </div>
+                <div class="space-x-2">
+                    <Link
+                        v-if="invoice.status === 'unpaid'"
+                        as="button"
+                        method="DELETE"
+                        :href="
+                            route('dashboard.merchant.invoices.cancel', {
+                                code: invoice.code,
+                            })
+                        "
+                        class="btn btn-outline-danger"
+                    >
+                        Batalkan
+                    </Link>
+                    <button
+                        v-if="midtransToken && invoice.status === 'unpaid'"
+                        class="btn btn-main"
+                        @click="createPayment"
+                    >
+                        Proses Pembayaran
+                        <FontAwesomeIcon :icon="faArrowRight" />
+                    </button>
+                </div>
+            </div>
+        </template>
+    </Container>
+</template>
+<script setup>
+import Container from "@/Components/Dashboard/UI/Container.vue";
+import { formatIDR } from "@/helpers/Dashboard/currency-format";
+import { formatDateID, formatDateTimeID } from "@/helpers/Dashboard/date";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { Head, Link, router } from "@inertiajs/vue3";
+import { onMounted } from "vue";
+
+const props = defineProps({
+    invoice: Object,
+    midtransClientKey: String,
+    midtransToken: String,
+});
+
+const createPayment = () => {
+    if (window.snap) {
+        window.snap.pay(props.midtransToken, {
+            onSuccess: function (result) {
+                router.reload();
+            },
+            onClose: function () {
+                alert("Anda menutup popup tanpa menyelesaikan pembayaran");
+            },
+        });
+    }
+};
+
+onMounted(() => {
+    if (!document.querySelector("#midtrans-snap")) {
+        const script = document.createElement("script");
+        script.id = "midtrans-snap";
+        script.src = "https://app.sandbox.midtrans.com/snap/snap.js";
+        script.setAttribute("data-client-key", props.midtransClientKey);
+        document.head.appendChild(script);
+    }
+});
+</script>
