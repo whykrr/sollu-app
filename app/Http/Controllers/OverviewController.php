791916bb19fb224e77d507e6f1944a82331cf811
Wahyu Kristiawan <whykris97@gmail.com>
2026-08-14 12:17:10 +0700

    fix: reorder use statements in OverviewController for consistency

--- app/Http/Controllers/OverviewController.php
+++ app/Http/Controllers/OverviewController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\Reports\DashboardService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-
-use App\Services\Reports\DashboardService;
 
 class OverviewController extends Controller
 {
