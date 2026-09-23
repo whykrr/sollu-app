7b03a551ba95b5987df34b95c88cb7f11e22b0d5
Wahyu Kristiawan <whykris97@gmail.com>
2026-09-23 19:47:27 +0700

    feat: configure server to ignore storage directory during file watching

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: ['**/storage/**'],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/cockpit.js'],
